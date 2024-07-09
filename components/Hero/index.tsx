602e30507016ec56e934c81ab079dfd20bde84d4
sunaj <sjinsta71@gmail.com>
2024-07-09 11:47:11 +0530

    changes

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -22,7 +22,7 @@ const Hero = () => {
               <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero">
                 Empower Your Business with
                 {' '}
-                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark" style={{ marginLeft: '0.5rem' }}>
+                <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full" style={{ marginLeft: '0.5rem' }}>
                   ZedsoftTech
                 </span>
               </h1>
