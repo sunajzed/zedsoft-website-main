aa07682c0972907db579d6e383ae335104f4f5c7
zedsofttech <zedsoft.in@gmail.com>
2024-08-02 17:08:42 +0530

    changes

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -1,10 +1,11 @@
+
 export default function Head() {
   return (
     <>
       <title> ZedsoftTech | Information Technology </title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Built with Next.js and TypeScript" />
-      {/* <link rel="icon" href="/images/favicon.ico" /> */}
+      <link rel="icon" href="../../public/images/logo/logowhite1.png" />
     </>
   );
 }
